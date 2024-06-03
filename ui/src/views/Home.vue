cfa33f41bb2c7648906a90b357223365351fce54
navtajr <2310781020@fh-burgenland.at>
2024-06-03 23:41:07 +0200

    Vue Router

---
+++ ui/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Nawi Tenant UI</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+</style>
